022f904b0b7464be364d387d472dfa72864740f5
Explo38 <144239380+Explo38@users.noreply.github.com>
2024-12-04 21:41:56 +0100

    ,hg

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Timer from './components/Timer/timer';
+import Timer from './components/Timer/timmer';
 import Rules from './components/Rules/rules';
 import ToggleSwitch from './components/ToggleSwitch/ToggleSwitch';
 
