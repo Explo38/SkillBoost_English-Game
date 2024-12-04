e7c6909b5878899aaaf1b96ebd014dcdc5d11863
Explo38 <144239380+Explo38@users.noreply.github.com>
2024-12-04 21:09:08 +0100

    ;h

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Timer from './components/Timer/timer';
-import Rules from './components/Rules/rules';
+import Timer from './components/Timer/Timer';
+import Rules from './components/Rules/Rules';
 import ToggleSwitch from './components/ToggleSwitch/ToggleSwitch';
 
 const App: React.FC = () => {
