2b4bcac127d5d952027f223af45d6e4bcb67a4cd
Cyrian <144239380+Explo38@users.noreply.github.com>
2024-12-05 16:36:59 +0100

    cqs

--- src/components/Rules/Rules.tsx
+++ src/components/Rules/Rules.tsx
@@ -1,42 +1,46 @@
 import React from 'react';
-import './Rules.css';
+import './Rules.css'; // Import du CSS pour appliquer le style
 
 const Rules: React.FC = () => {
   return (
     <div className="rules-wrapper">
       <div className="rules-container">
-        <h1>GAME RULES</h1>
+        <h1>Game rules</h1>
+        <h2>The goal of this game :</h2>
+        <p>Try to earn and improve your soft skills through this game, and boost your general capacity</p>
 
-        <h2>THE GOAL OF THIS GAME:</h2>
+        <h2>Set up :</h2>
         <p>
-          Try to earn and improve your soft skills through this game, and boost your general capacity.
+          Put the board game on a table, take all the “soft skills” card and lay it out next to the board game by name
+          and levels, each player choose his pawn. <br />
+          The add-ons will helps you to have an access to the timer for some challenges
         </p>
 
-        <h2>SET UP:</h2>
-        <p>
-          Put the board game on a table, take all the “soft skills” cards by name and lay them out next to the board game. Each player chooses their pawn.
-        </p>
-        <p>
-          The add-ons will help you have access to the timer for some challenges.
-        </p>
-
-        <h2>HOW TO PLAY:</h2>
-          <li>The youngest player has to begin.</li>
-          <li>Roll the dice and move forward by the number indicated on it.</li>
+        <h2>How to play :</h2>
+        <ul>
+          <li>The youngest player has to begin</li>
+          <li>Roll the dice and move forward of the numbers that are indicated on it.</li>
           <li>
-            When you land on a soft skill box, take the card indicated (name + easy or hard) and let another player ask you the question. If it’s a challenge card, perform the challenge or you will lose a soft skills card!
+            When you are on a soft skill box, take the card indicated (name + easy or hard) and let another player ask
+            you the question or if it’s a challenge card, do it, or you will lose a soft skills card !
           </li>
-          <li>If you answer correctly, you earn the card. If not, you don’t.</li>
-          <li>The game ends when one player has earned 5 soft skills cards.</li>
+          <li>If you answer right, you earn the card. If not, you don’t.</li>
+          <li>The game is ending when one player has earnt 5 soft skills cards.</li>
+        </ul>
 
-        <h2>SPECIAL CASE:</h2>
+        <h2>Special case :</h2>
+        <ul>
           <li>
-            If you land on a soft skill that you have already won, you can choose any soft skill card and either perform the challenge or answer the question.
+            If you fall on a soft skill that you have already won, you can choose any soft skill card and realize the
+            challenge or answer the question.
           </li>
-          <li>Every time you pass the START (a turn of a set), you win 1 soft skills card.</li>
+          <li>
+            Every time you pass to the START (a turn of a set), you win 1 soft skills cards
+          </li>
+        </ul>
       </div>
     </div>
   );
-}
+};
 
 export default Rules;
