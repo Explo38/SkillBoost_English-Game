aba8bf130a56bda22d8dfe2d5b473f2285a618a5
Explo38 <144239380+Explo38@users.noreply.github.com>
2024-12-05 21:30:49 +0100

    ngf

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './footer.css'; // Assurez-vous d'avoir lié le CSS correctement
 
-const Footer: React.FC = () => {
-  return (
-<footer className="footer">
-  <span className="footer-text">Team Rocket©️</span>
-</footer>
+// Importation de l'image
+import image from '../../assets/Annecy.jpg'; // Remplace ce chemin par celui de ton image
 
+const Footer: React.FC = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  // Fonction pour ouvrir/fermer la pop-up
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
+  return (
+    <footer className="footer">
+      <span className="footer-text" onClick={toggleModal}>Team Rocket©️</span>
+
+      {/* Pop-up */}
+      {isModalOpen && (
+        <div className="modal-overlay">
+          <div className="modal">
+            <button className="close-btn" onClick={toggleModal}>✖</button>
+            <div className="modal-content">
+              <img src={image} alt="Description" className="modal-image" />
+              <p className="modal-text">English Game 2024 MDS</p>
+              <p> #bestcrazyteam🤙
+              </p>
+            </div>
+          </div>
+        </div>
+      )}
+    </footer>
   );
 };
 
